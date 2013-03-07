8ce0178ee0852185c04649e7490ed3270ce70efa
devasur <boni.gopalan@gmail.com>
2013-03-07 16:46:02 +0530

    parameters for deployed application.

--- myapp.js
+++ myapp.js
@@ -5,9 +5,7 @@ exports.clientsecret = "CubzjwzGyutnnrApHDBvnCrGwEovzsDd";
 exports.scopes = [
     "access",
     "user_balance",
-    "user_email",
-    "user_anon_lender_data",
-    "user_loan_balances"
+    "user_email"
   ];
 
-exports.callbackURL = "http://test.bonigopalan.com:3000/auth/kiva/callback";
+exports.callbackURL = "http://kivaoauth.aws.af.cm/auth/kiva/callback";
