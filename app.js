de914d3c1c7aa422cbad380b10b0361488fce64a
devasur <boni.gopalan@gmail.com>
2013-03-07 16:29:52 +0530

    complete login, logout, check resource flow with some decent UI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
+  app.use(express.static(__dirname + '/public'));
   app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.session({ secret: 'kiva has lots of secrets' }));
@@ -9,17 +10,58 @@ app.configure(function() {
 });
 
 app.get('/', function(req, resp) {
-	resp.sendfile('authenticatetokiva.html');
+	if(req.session.user == undefined){
+		resp.sendfile(__dirname  + '/public/authenticatetokiva.html');
+	}
+	else{
+		resp.sendfile(__dirname  + '/public/checkstuff.html')
+	}
+});
+
+app.get('/logout', function(req, resp) {
+	delete(req.session.user);
+	resp.redirect('/');
 });
 
 var kivaoAuth = new (require('./kivaoAuth.js')).$();
+
 app.get('/auth/kiva', function(req,resp){
-	kivaoAuth.getOAuthRequestToken(req,resp);
+	if(req.session.user == undefined){
+		kivaoAuth.getOAuthRequestToken(req,resp);
+	}
+	else{
+		console.log("User Already Logged in")
+		resp.sendfile(__dirname  + '/public/checkstuff.html')
+	}
+
 });
+
+app.get('/welcome',function(req,resp){
+	if(req.session.user == undefined){
+		resp.send("Not authorized",401);
+		return;
+	}
+	var user = req.session.user.user_account;
+	resp.send({'message': user.first_name + " " + user.last_name})
+})
+
+app.get('/balance',function(req,resp){
+		if(req.session.user == undefined){
+			resp.send("Not authorized",401);
+			return;
+		}
+		console.log(req.session.user.access);
+		kivaoAuth.getBalance(req.session.user.access,function(data){
+			console.log(data);
+			resp.send(data);
+		});
+})
+
 app.get('/auth/kiva/callback', 
 	function (req, resp){
 		kivaoAuth.getOAuthAccessToken(req,resp,function(data){
-			resp.send(data);
+			req.session.user = data;
+			resp.sendfile(__dirname  + '/public/checkstuff.html')
 		});
 	}
 );
