b8b4406362040a008d3e5e5b5306c8e0c590a30f
devasur <boni.gopalan@gmail.com>
2013-03-07 18:04:33 +0530

    added footer with links to Github:

--- myapp.js
+++ myapp.js
@@ -9,3 +9,21 @@ exports.scopes = [
   ];
 
 exports.callbackURL = "http://kivaoauth.aws.af.cm/auth/kiva/callback";
+
+/*
+//For easy troubleshooting on localhost
+exports.appid = "com.bonigopalan.test";
+exports.clientid="com.bonigopalan.test";
+exports.clientsecret = "pxIvokqHsvogHdvjDCArwyqvdxrMEkrC";
+
+exports.scopes = [
+    "access",
+    "user_balance",
+    "user_email",
+    "user_anon_lender_data",
+    "user_loan_balances"
+  ];
+
+exports.callbackURL = "http://test.bonigopalan.com:3000/auth/kiva/callback";
+
+*/
